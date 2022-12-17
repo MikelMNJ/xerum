e50f8e699b463d3cae3f37c1ffbbf23058bc7cb3
Mikel Jagan <mikelmnj@gmail.com>
2022-12-17 14:25:14 -0500

    Added font-awesome to dev environment.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './scenes/App';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const MyApp = (
   <StrictMode>
